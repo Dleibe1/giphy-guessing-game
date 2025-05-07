6a50652dd373db2485811ea218d4be0004c3d12a
dleibe1 <daniel.leibensperger28@gmail.com>
2025-05-07 11:49:59 -0400

    committing changes

--- backend/api_app/urls.py
+++ backend/api_app/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path, include
-from rest_framework import routers
+from rest_framework.routers import DefaultRouter
 from api_app.views import SentenceViewSet
 
-router = routers.DefaultRouter()
+router = DefaultRouter()
 router.register(r'sentence', SentenceViewSet)
 
 urlpatterns = [
 	path("", include(router.urls))
 ]
-
