ccc31c7bc00bc820a0516317ec3c70456f112e35
dleibe1 <daniel.leibensperger28@gmail.com>
2025-05-07 11:27:56 -0400

    creating new branch to configure eslint

---
+++ frontend/src/services/ApiService.ts
@@ -0,0 +1,10 @@
+export const getSentence = async (id: number) => {
+	try {
+		const response = await fetch(`/api/v1/sentence/${id}`)
+		if (!response.ok) {
+			const errorMessage = `${response.status} (${response.statusText})`
+			const error = new Error(errorMessage)
+			throw error
+		}
+	} catch (error) {}
+}
